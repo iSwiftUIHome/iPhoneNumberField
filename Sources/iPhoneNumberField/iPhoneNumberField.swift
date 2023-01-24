04055b8ca148b8373bcbd84837754b70316ca05a
Benjamin Sage <bls@MacBook-Ben.hsd1.ca.comcast.net>
2023-01-23 21:21:49 -0800

    Layer

--- Sources/iPhoneNumberField/iPhoneNumberField.swift
+++ Sources/iPhoneNumberField/iPhoneNumberField.swift
@@ -201,8 +201,7 @@ public struct iPhoneNumberField: UIViewRepresentable {
             gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
             gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
             
-            uiView.layer.addSublayer(gradient)
-            
+            gradient.mask = uiView.layer
         }
 
         if isFirstResponder {
